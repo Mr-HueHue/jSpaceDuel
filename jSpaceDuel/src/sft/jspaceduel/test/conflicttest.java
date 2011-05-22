faa20299cf0a11e75f4cbbe036aef30df86a7a41
Jonas Jelten <jonas.jelten@gmail.com>
2011-05-22 15:08:40 +0200

    git conflict test two

---
+++ jSpaceDuel/src/sft/jspaceduel/test/conflicttest.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sft.jspaceduel.test;
+
+/**
+ *
+ * @author JJ
+ */
+public class conflicttest {
+    public static void main(String[] args) {
+        System.out.println("Conflict line two.");
+    }
+}
