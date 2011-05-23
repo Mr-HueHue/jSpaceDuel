b32352cd51d448002cdfedf207afd4090b505c9e
mic-e <michael.ensslin@gmail.com>
2011-05-24 00:22:35 +0200

    SpaceObjects bearbeitet.

---
+++ jSpaceDuel/src/sft/jspaceduel/spaceObjects/SpaceCraft.java
@@ -0,0 +1,14 @@
+package sft.jspaceduel.spaceObjects;
+
+import sft.jspaceduel.spaceObjects.SpaceObject;
+
+/**
+ *
+ * @author JJ
+ */
+public class SpaceCraft extends SpaceObject {
+    public SpaceCraft(double[] pos, double[] v, double mass, double angle, double angularVelocity) {
+        super(pos, v, mass, angle, angularVelocity);
+    }
+    
+}
